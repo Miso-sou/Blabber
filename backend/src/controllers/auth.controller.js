3b5f55fa954171e11907eacec04c65e25153600c
Miso-sou <work.wadhwani@gmail.com>
2025-10-05 04:40:28 +0530

    First Big commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req, res){
+    res.send("Signup Route")
+}
+
+export async function login(rreq, res){
+    res.send("Login Route")
+}
+
+export function logout(req, res){
+    res.send("Logout Route")
+}
