0acd560b673c3141d991ab0cd3a8562c13db4b40
Miso-sou <work.wadhwani@gmail.com>
2025-10-06 01:37:20 +0530

    Login and Logout controller created successfully with postman checking

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -42,10 +42,39 @@ export async function signup(req, res){
     }
 }
 
-export async function login(rreq, res){
-    res.send("Login Route")
+export async function login(req, res){
+    try {
+        const {email, password} = req.body
+
+        if(!email || !password){
+            return res.status(400).json({message: "All fields are required"})
+        }
+
+        const user = await User.findOne({email})
+        if(!user) return res.status(401).json({message: "Invalid email or password"})
+
+        const isPasswordCorrect = await user.matchPassword(password)
+
+        if(!isPasswordCorrect) return res.status(401).json({message: "Invalid email or password"})
+
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET_KEY, {expiresIn: '7d'})
+
+        res.cookie("jwt", token, {
+            maxAge: 7*24*60*60*1000, // 7 days
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict"
+        })
+
+        res.status(200).json({success: true, user})
+
+    } catch (error) {
+        console.log("Error in Login controller", error.message);
+        res.status(500).json({message: "Internal server error"})
+    }
 }
 
 export function logout(req, res){
-    res.send("Logout Route")
+    res.clearCookie("jwt")
+    res.status(200).json({success: true, message: "Logout succesfull"})
 }
