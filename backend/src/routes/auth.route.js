9bf25d6b90f127e714070cb95b4814e4ab9f1663
Miso-sou <work.wadhwani@gmail.com>
2025-10-08 02:08:25 +0530

    added user routes and controllers for recommended friends and fetch friends of a user

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,13 +2,17 @@ import express from "express"
 import {signup, login, logout, onboard} from "../controllers/auth.controller.js"
 import { protectRoute } from "../middleware/auth.middleware.js";
 
-
 const router = express.Router();
 
 router.post("/signup", signup)
 router.post("/login", login)
 router.post("/logout", logout)
 router.post("/onboarding", protectRoute, onboard)
+
+// Checks if the user is logged in or not
+router.get("/me", protectRoute, (req, res) => {
+    res.status(200).json({success: true, user: req.user})
+})
 
 
 
---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { protectRoute } from "../middleware/auth.middleware.js"
+import { getMyFriends, getRecommendedUsers } from "../controllers/user.controller.js"
+
+const router = express.Router()
+router.use(protectRoute) // apply auth middleware to all routes
+
+router.get("/", getRecommendedUsers)
+router.get("/friends", getMyFriends)
+
+export default router
