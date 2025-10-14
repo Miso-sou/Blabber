6581e2deb512d2413c4f87f75db6845be409384e
Miso-sou <work.wadhwani@gmail.com>
2025-10-15 02:01:28 +0530

    Created the signup page

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"
+
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
@@ -11,6 +13,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow cookies to be sent
+}))
 app.use(express.json());
 app.use(cookieParser());
 
